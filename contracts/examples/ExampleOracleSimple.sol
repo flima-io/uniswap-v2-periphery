258f63686815bca02e3884b8d81789e642426f19
Kars <kars@flima.io>
2022-11-11 19:47:16 +0700

    update compiler version

--- contracts/examples/ExampleOracleSimple.sol
+++ contracts/examples/ExampleOracleSimple.sol
@@ -1,4 +1,4 @@
-pragma solidity =0.6.6;
+pragma solidity ^0.8.4;
 
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
--- contracts/interfaces/V1/IUniswapV1Factory.sol
+++ contracts/interfaces/V1/IUniswapV1Factory.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0;
+pragma solidity ^0.8.4;
 
 interface IUniswapV1Factory {
     function getExchange(address) external view returns (address);
